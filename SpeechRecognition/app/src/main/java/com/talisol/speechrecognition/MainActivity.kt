5a61ac66ae8536c7a620b8afc68e0c49aa8194b5
aoussou <aoussou@talisol.com>
2022-01-07 15:33:10 +0900

    able to read model

--- SpeechRecognition/app/src/main/java/com/talisol/speechrecognition/MainActivity.kt
+++ SpeechRecognition/app/src/main/java/com/talisol/speechrecognition/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.talisol.speechrecognition
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -9,30 +11,54 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.talisol.speechrecognition.ui.theme.SpeechRecognitionTheme
+import org.pytorch.LiteModuleLoader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = MainActivity::class.java.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var module = LiteModuleLoader.load(assetFilePath(getApplicationContext(), "wav2vec2.ptl"))
+
         setContent {
             SpeechRecognitionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpeechRecognitionTheme {
-        Greeting("Android")
+    private fun assetFilePath(context: Context, assetName: String): String? {
+        val file = File(context.filesDir, assetName)
+        if (file.exists() && file.length() > 0) {
+            return file.absolutePath
+        }
+        try {
+            context.assets.open(assetName).use { `is` ->
+                FileOutputStream(file).use { os ->
+                    val buffer = ByteArray(4 * 1024)
+                    var read: Int
+                    while (`is`.read(buffer).also { read = it } != -1) {
+                        os.write(buffer, 0, read)
+                    }
+                    os.flush()
+                }
+                return file.absolutePath
+            }
+        } catch (e: IOException) {
+            Log.e(TAG, assetName + ": " + e.getLocalizedMessage())
+        }
+        return null
     }
 }
+
+
